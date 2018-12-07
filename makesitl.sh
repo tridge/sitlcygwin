e3ff98bb9fb359d4cc27eee19488db90afb611e8
Michael Oborne <mich146@hotmail.com>
2018-12-07 20:40:04 +0800

    Update makesitl.sh

--- makesitl.sh
+++ makesitl.sh
@@ -2,8 +2,6 @@
 
 PATH="/usr/local/bin:/usr/bin:/bin:$PATH"
 
-set EXTRAFLAGS = -DHAVE_CMATH_ISFINITE -DNEED_CMATH_ISFINITE_STD_NAMESPACE -DHAVE_CMATH_ISINF -DNEED_CMATH_ISINF_STD_NAMESPACE -DHAVE_CMATH_ISNAN -DNEED_CMATH_ISNAN_STD_NAMESPACE
-
 cd /cygdrive/C/cygwin/home/media/ardupilot/
 
 git reset --hard origin/master
@@ -12,33 +10,10 @@ git checkout -f master 2>&1 | tee /cygdrive/C/cygwin/home/media/sitl/git.txt
 git pull 2>&1 | tee /cygdrive/C/cygwin/home/media/sitl/git.txt
 git submodule update --init --recursive -f 2>&1 | tee -a /cygdrive/C/cygwin/home/media/sitl/git.txt
 
-git checkout -f APMrover2-stable 2>&1 | tee /cygdrive/C/cygwin/home/media/sitl/git.txt
-cd APMrover2
-make clean
-make sitl -j8 -i 2>&1 | tee /cygdrive/C/cygwin/home/media/sitl//APMrover2.txt
-strip /tmp/APMrover2.build/APMrover2.elf
-cp /tmp/APMrover2.build/APMrover2.elf /cygdrive/C/cygwin/home/media/sitl/
-cd ..
-# next
-git checkout -f ArduCopter-stable 2>&1 | tee /cygdrive/C/cygwin/home/media/sitl/git.txt
-cd ArduCopter
-make clean
-make sitl -j8 -i 2>&1 | tee /cygdrive/C/cygwin/home/media/sitl//ArduCopter.txt
-strip /tmp/ArduCopter.build/ArduCopter.elf
-cp /tmp/ArduCopter.build/ArduCopter.elf /cygdrive/C/cygwin/home/media/sitl/
-make clean
-make sitl-heli  -j8 -i 2>&1 | tee /cygdrive/C/cygwin/home/media/sitl//Heli.txt
-strip /tmp/ArduCopter.build/ArduCopter.elf
-cp /tmp/ArduCopter.build/ArduCopter.elf /cygdrive/C/cygwin/home/media/sitl/ArduHeli.elf
-cd ..
-# next
-git checkout -f ArduPlane-stable 2>&1 | tee /cygdrive/C/cygwin/home/media/sitl/git.txt
-cd ArduPlane
-make clean
-make sitl -j8 -i 2>&1 | tee /cygdrive/C/cygwin/home/media/sitl//ArduPlane.txt
-strip /tmp/ArduPlane.build/ArduPlane.elf
-cp /tmp/ArduPlane.build/ArduPlane.elf /cygdrive/C/cygwin/home/media/sitl/
-cd ..
+./waf configure | tee /cygdrive/C/cygwin/home/media/sitl/configure.txt
+./waf build | tee /cygdrive/C/cygwin/home/media/sitl/build.txt
+
+cp /cygdrive/C/cygwin/home/media/ardupilot/build/sitl/bin/ /cygdrive/C/cygwin/home/media/sitl/
 
 cd /cygdrive/C/cygwin/home/media/sitl/
 ls
