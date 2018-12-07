dd019f2d0cb9e3d269a3eed6d6d2d25011ae35d9
Michael Oborne <mich146@hotmail.com>
2018-12-08 07:26:00 +0800

    Update makesitl.sh

--- makesitl.sh
+++ makesitl.sh
@@ -2,20 +2,20 @@
 
 PATH="/usr/local/bin:/usr/bin:/bin:$PATH"
 
-cd /work/ardupilot/
+cd /c/cygdrive/work/ardupilot/
 
 git reset --hard origin/master
 git clean -f
-git checkout -f master 2>&1 | tee /work/sitl/git.txt
-git pull 2>&1 | tee /work/sitl/git.txt
-git submodule update --init --recursive -f 2>&1 | tee -a /work/sitl/git.txt
+git checkout -f master 2>&1 | tee /c/cygdrive/work/sitl/git.txt
+git pull 2>&1 | tee /c/cygdrive/work/sitl/git.txt
+git submodule update --init --recursive -f 2>&1 | tee -a /c/cygdrive/work/sitl/git.txt
 
-./waf configure | tee /work/sitl/configure.txt
-./waf build | tee /work/sitl/build.txt
+./waf configure | tee /c/cygdrive/work/sitl/configure.txt
+./waf build | tee /c/cygdrive/work/sitl/build.txt
 
-cp /work/ardupilot/build/sitl/bin/ /work/sitl/
+cp /c/cygdrive/work/ardupilot/build/sitl/bin/ /c/cygdrive/work/sitl/
 
-cd /work/sitl/
+cd /c/cygdrive/work/sitl/
 ls
 
 
