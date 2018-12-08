c70961479aad5a97abf3ddda1a49be2916f497c9
Michael Oborne <mich146@hotmail.com>
2018-12-08 10:23:34 +0800

    Update makesitl.sh

--- makesitl.sh
+++ makesitl.sh
@@ -16,7 +16,14 @@ git submodule update --init --recursive -f 2>&1 | tee -a /cygdrive/c/work/sitl/g
 ./waf -j4 rover | tee /cygdrive/c/work/sitl/rover.txt
 ./waf -j4 heli | tee /cygdrive/c/work/sitl/heli.txt
 
-cp -r /cygdrive/c/work/ardupilot/build/sitl/bin/ /cygdrive/c/work/sitl/
+cp /cygdrive/c/work/ardupilot/build/sitl/bin/ardurover.exe /cygdrive/c/work/sitl/APMrover2.elf
+cp /cygdrive/c/work/ardupilot/build/sitl/bin/arduplane.exe /cygdrive/c/work/sitl/ArduPlane.elf
+cp /cygdrive/c/work/ardupilot/build/sitl/bin/arducopter.exe /cygdrive/c/work/sitl/ArduCopter.elf
+cp /cygdrive/c/work/ardupilot/build/sitl/bin/arducopter-heli.exe /cygdrive/c/work/sitl/ArduHeli.elf
+
+cp /bin/cyggcc_s-1.dll /cygdrive/c/work/sitl/cyggcc_s-1.dll
+cp /bin/cygstdc++-6.dll /cygdrive/c/work/sitl/cygstdc++-6.dll
+cp /bin/cygwin1.dll /cygdrive/c/work/sitl/cygwin1.dll
 
 cd /cygdrive/c/work/sitl/
 ls
