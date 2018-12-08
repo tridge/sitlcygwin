1ee7c107dfa160456c572ab5ac64ff4c7ffc25eb
Michael Oborne <mich146@hotmail.com>
2018-12-08 10:04:48 +0800

    Update makesitl.sh

--- makesitl.sh
+++ makesitl.sh
@@ -11,10 +11,10 @@ git pull 2>&1 | tee /cygdrive/c/work/sitl/git.txt
 git submodule update --init --recursive -f 2>&1 | tee -a /cygdrive/c/work/sitl/git.txt
 
 ./waf configure | tee /cygdrive/c/work/sitl/configure.txt
-./waf build -j4 copter | tee /cygdrive/c/work/sitl/copter.txt
-./waf build -j4 plane | tee /cygdrive/c/work/sitl/plane.txt
-./waf build -j4 rover | tee /cygdrive/c/work/sitl/rover.txt
-./waf build -j4 heli | tee /cygdrive/c/work/sitl/heli.txt
+./waf -j4 copter | tee /cygdrive/c/work/sitl/copter.txt
+./waf -j4 plane | tee /cygdrive/c/work/sitl/plane.txt
+./waf -j4 rover | tee /cygdrive/c/work/sitl/rover.txt
+./waf -j4 heli | tee /cygdrive/c/work/sitl/heli.txt
 
 cp -r /cygdrive/c/work/ardupilot/build/sitl/bin/ /cygdrive/c/work/sitl/
 
