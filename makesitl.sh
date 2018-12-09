b501855f7df423a22fc56e7c90cfc03374b4ff1a
Michael Oborne <mich146@hotmail.com>
2018-12-10 06:46:09 +0800

    add x64dll

--- makesitl.sh
+++ makesitl.sh
@@ -22,6 +22,7 @@ cp /cygdrive/c/work/ardupilot/build/sitl/bin/arducopter.exe /cygdrive/c/work/sit
 cp /cygdrive/c/work/ardupilot/build/sitl/bin/arducopter-heli.exe /cygdrive/c/work/sitl/ArduHeli.elf
 
 cp /bin/cyggcc_s-1.dll /cygdrive/c/work/sitl/cyggcc_s-1.dll
+cp /bin/cyggcc-1.dll /cygdrive/c/work/sitl/cyggcc-1.dll
 cp /bin/cygstdc++-6.dll /cygdrive/c/work/sitl/cygstdc++-6.dll
 cp /bin/cygwin1.dll /cygdrive/c/work/sitl/cygwin1.dll
 
