d17c0db149ab0ee68a2b74ff62e82a02e7298cb5
Andrew Tridgell <andrew@tridgell.net>
2018-12-10 10:18:25 +1100

    fixed 32 bit waf build
    using cygwin64, but with 32 bit compiler

--- makesitl.sh
+++ makesitl.sh
@@ -1,32 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-PATH="/usr/local/bin:/usr/bin:/bin:$PATH"
+export PATH="/usr/local/bin:/usr/bin:/bin"
+export PATH
+
+set -x
 
 cd /cygdrive/c/work/ardupilot/
 
 git reset --hard origin/master
 git clean -f
-git checkout -f master 2>&1 | tee /cygdrive/c/work/sitl/git.txt
-git pull 2>&1 | tee /cygdrive/c/work/sitl/git.txt
-git submodule update --init --recursive -f 2>&1 | tee -a /cygdrive/c/work/sitl/git.txt
+git checkout -f master 2>&1
+git pull
+git submodule update --init --recursive -f
 
-./waf configure | tee /cygdrive/c/work/sitl/configure.txt
-./waf -j4 copter | tee /cygdrive/c/work/sitl/copter.txt
-./waf -j4 plane | tee /cygdrive/c/work/sitl/plane.txt
-./waf -j4 rover | tee /cygdrive/c/work/sitl/rover.txt
-./waf -j4 heli | tee /cygdrive/c/work/sitl/heli.txt
+# build for 32 bit target
+CXX=i686-pc-cygwin-g++.exe CC=i686-pc-cygwin-gcc ./waf configure --board sitl
+
+cat build/config.log
+/usr/bin/python waf -j4 copter
+/usr/bin/python waf -j4 plane
+/usr/bin/python waf -j4 rover
+/usr/bin/python waf -j4 heli
 
 cp /cygdrive/c/work/ardupilot/build/sitl/bin/ardurover.exe /cygdrive/c/work/sitl/APMrover2.elf
 cp /cygdrive/c/work/ardupilot/build/sitl/bin/arduplane.exe /cygdrive/c/work/sitl/ArduPlane.elf
 cp /cygdrive/c/work/ardupilot/build/sitl/bin/arducopter.exe /cygdrive/c/work/sitl/ArduCopter.elf
 cp /cygdrive/c/work/ardupilot/build/sitl/bin/arducopter-heli.exe /cygdrive/c/work/sitl/ArduHeli.elf
 
-cp /bin/cyggcc_s-1.dll /cygdrive/c/work/sitl/cyggcc_s-1.dll
-cp /bin/cyggcc-1.dll /cygdrive/c/work/sitl/cyggcc-1.dll
-cp /bin/cygstdc++-6.dll /cygdrive/c/work/sitl/cygstdc++-6.dll
-cp /bin/cygwin1.dll /cygdrive/c/work/sitl/cygwin1.dll
+cp /usr/i686-pc-cygwin/sys-root/usr/bin/*.dll /cygdrive/c/work/sitl/
 
 cd /cygdrive/c/work/sitl/
 ls
-
-
