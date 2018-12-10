8565c468d50b09b71be1029638099da0c5206f58
Andrew Tridgell <andrew@tridgell.net>
2018-12-10 11:12:44 +1100

    slim down build and show config.log

--- makesitl.sh
+++ makesitl.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 PATH="/usr/local/bin:/usr/bin:/bin:$PATH"
+
+set -x
 
 cd /cygdrive/c/work/ardupilot/
 
@@ -10,11 +12,12 @@ git checkout -f master 2>&1 | tee /cygdrive/c/work/sitl/git.txt
 git pull 2>&1 | tee /cygdrive/c/work/sitl/git.txt
 git submodule update --init --recursive -f 2>&1 | tee -a /cygdrive/c/work/sitl/git.txt
 
-./waf configure | tee /cygdrive/c/work/sitl/configure.txt
-./waf -j4 copter | tee /cygdrive/c/work/sitl/copter.txt
-./waf -j4 plane | tee /cygdrive/c/work/sitl/plane.txt
-./waf -j4 rover | tee /cygdrive/c/work/sitl/rover.txt
-./waf -j4 heli | tee /cygdrive/c/work/sitl/heli.txt
+./waf configure --board sitl
+cat build/config.log
+./waf -j4 copter
+./waf -j4 plane
+./waf -j4 rover
+./waf -j4 heli
 
 cp /cygdrive/c/work/ardupilot/build/sitl/bin/ardurover.exe /cygdrive/c/work/sitl/APMrover2.elf
 cp /cygdrive/c/work/ardupilot/build/sitl/bin/arduplane.exe /cygdrive/c/work/sitl/ArduPlane.elf
