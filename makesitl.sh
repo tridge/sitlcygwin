5711a6bdb59c661d07da84e67e60ff2bbaf36df0
Andrew Tridgell <andrew@tridgell.net>
2018-12-10 13:05:46 +1100

    back to using cygwin64
    but with 32 bit compiler

--- makesitl.sh
+++ makesitl.sh
@@ -5,12 +5,6 @@ export PATH
 
 set -x
 
-type -all g++
-g++ -v || exit 1
-
-type python
-python --version
-
 cd /cygdrive/c/work/ardupilot/
 
 git reset --hard origin/master
@@ -19,13 +13,9 @@ git checkout -f master 2>&1
 git pull
 git submodule update --init --recursive -f
 
-ls -l /usr/bin/g++
-/usr/bin/g++ -v || exit 1
-echo | /usr/bin/g++ -dM -E -
+# build for 32 bit target
+CXX=i686-pc-cygwin-g++.exe CC=i686-pc-cygwin-gcc ./waf configure --board sitl
 
-/usr/bin/python --version
-
-/usr/bin/python waf configure --board sitl
 cat build/config.log
 /usr/bin/python waf -j4 copter
 /usr/bin/python waf -j4 plane
@@ -37,12 +27,7 @@ cp /cygdrive/c/work/ardupilot/build/sitl/bin/arduplane.exe /cygdrive/c/work/sitl
 cp /cygdrive/c/work/ardupilot/build/sitl/bin/arducopter.exe /cygdrive/c/work/sitl/ArduCopter.elf
 cp /cygdrive/c/work/ardupilot/build/sitl/bin/arducopter-heli.exe /cygdrive/c/work/sitl/ArduHeli.elf
 
-cp /bin/cyggcc_s-1.dll /cygdrive/c/work/sitl/cyggcc_s-1.dll
-cp /bin/cyggcc-1.dll /cygdrive/c/work/sitl/cyggcc-1.dll
-cp /bin/cygstdc++-6.dll /cygdrive/c/work/sitl/cygstdc++-6.dll
-cp /bin/cygwin1.dll /cygdrive/c/work/sitl/cygwin1.dll
+cp /usr/i686-pc-cygwin/sys-root/usr/bin/*.dll /cygdrive/c/work/sitl/
 
 cd /cygdrive/c/work/sitl/
 ls
-
-
